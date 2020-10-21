ff2b77dbac5d76553e704fd242e01555f318571e
abhijitrajput <abhijit27789@gmail.com>
2020-10-21 18:43:07 +0530

    laravel api update

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return csrf_token();
+        $students = Student::get();
+        return response($students, 200);
+        //return "Hello there!!!";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $student = new Student;
+        $student->name = $request->name;
+        $student->course = $request->course;
+        $student->save();
+
+        return response()->json([
+            "message" => "student record created"
+        ], 201);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        if (Student::where('id', $id)->exists()) {
+            $students = Student::find($id);
+            return response($students, 200);
+        } else {
+            return response()->json([
+                "message" => "student not found."
+            ],404);
+        }
+
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $students = Student::find($id);
+        $students->name = $request->get('name');
+        $students->course = $request->get('course');
+        $students->save();
+        return response()->json([
+            "message" => "student updated successfully."
+        ]);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //return csrf_token();
+        $students = Student::find($id);
+        $students->delete();
+        return response()->json([
+            "message" => "student has been deleted"
+        ],200);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Student;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('students',[ApiController::class, 'index']);
+Route::get('students/{id}',[ApiController::class, 'show']);
+Route::post('students/create',[ApiController::class, 'create']);
+Route::put('students/{id}',[ApiController::class, 'update']);
+Route::delete('students/{id}', [ApiController::class, 'destroy']);
+
